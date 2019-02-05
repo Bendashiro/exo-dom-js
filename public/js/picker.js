f2f9e2d9cbb88c246c9121cf6c1070efea87c08f
Bendashiro <hich.b1997@gmail.com>
2019-02-05 11:19:14 +0100

    picker 95%

--- public/js/picker.js
+++ public/js/picker.js
@@ -1,37 +1,45 @@
 //Import html
+/*
 var bgColor = document.getElementsByClassName('bg-tar');
 var pickColor = document.getElementById('color');
 var borStyle = document.getElementsByClassName('bor-style');
-var borWei = document.getElementById('bor-wei');
-var borRadius = document.getElementById('bor-rad');
+var borWei = document.getElementsByClassName('bor-wei');
+var borRadius = document.getElementsByClassName('bor-rad');
+*///variable sans utilisation de data-set
+//pour data-set
+var buttons = document.getElementsByTagName('button');
+var inputs = document.getElementsByTagName('input');
 var targetDiv = document.getElementById('target');
-console.log(borWei);
-console.log(borWei);
 
 //Fonction utilisable
 
+const clickStyle = (elem) => {
+    let prop = elem.dataset.style;//on recupere les propriété css avec le data-set
+    let cssElem = window.getComputedStyle(elem);//on recupere la la valeur de la prop assigné
+    let value = cssElem[prop];
+    targetDiv.style[prop] = value;
+}
+const inputStyle = (elem) => {
+    let prop = elem.dataset.style;
+    let cssElem = window.getComputedStyle(elem);
+    let value = cssElem[prop];
+    targetDiv.style[prop] = isNaN(elem.value) ? elem.value : `${elem.value}px`
+}
 
 //Ecouteur d'evenement
-pickColor.addEventListener('change',function(event) {
-    let inputColor = event.target.value;
-    bgColor[1].style.backgroundColor = inputColor;
-});
-for(let i = 0; i < bgColor.length;i++){
-    bgColor[i].addEventListener('click',function() {
-        let color = bgColor[i].style.backgroundColor;
-        targetDiv.style.backgroundColor = color;
+for(let i = 0; i < buttons.length;i++){
+    buttons[i].addEventListener('click',(event) => {
+        clickStyle(event.target);
     });
+};
+for(let i2 = 0;i2< inputs.length;i2++){
+    inputs[i2].addEventListener('input',(event) => {
+        inputStyle(event.target);
+    })
 }
-for(let i2 = 0; i2 < borStyle.length; i2++){
-    borStyle[i2].addEventListener('click', function() {
-        let border = window.getComputedStyle(borStyle[i2]).borderStyle;//getComputedStyle s'utlise via la fenetre et on précise l'element ciblé
-        console.log('le click est prit en compte')
-        targetDiv.style.borderStyle = border;
-    });
-}
-borWei.addEventListener('input',function() {
-    targetDiv.style.borderWidth = `${borWei.value}px`;
-});
-borRadius.addEventListener('input',function() {
-    targetDiv.style.borderRadius = `${borRadius.value}%`;
-});
+// borWei.addEventListener('input',function() {
+//     targetDiv.style.borderWidth = `${borWei.value}px`;
+// });
+// borRadius.addEventListener('input',function() {
+//     targetDiv.style.borderRadius = `${borRadius.value}%`;
+// });
